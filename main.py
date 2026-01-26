b70717181e9c82b5099f53ac4d335066eb36f223
Elliot Thurlow <snuggyizme@gmail.com>
2026-01-26 16:47:38 +1100

    Hell

--- main.py
+++ main.py
@@ -6,43 +6,45 @@ import pyperclip
 import FreeSimpleGUI as fsGUI
 fsGUI.theme("LightGrey1")
 
-homeLayout = [[fsGUI.Text("IPAt")],
-              [fsGUI.Button("Import/Create a language config"), fsGUI.Button("Create IPA for words"), fsGUI.Button("Close")]]
+def createHomeLayout():
+    return [[fsGUI.Text("IPAt")],
+            [fsGUI.Button("Import/Create a language config"),
+             fsGUI.Button("Create IPA for words"),
+             fsGUI.Button("Close")]]
 
-importOrCreateLayout = [
-    [fsGUI.Button("Import (Paste from clipboard)")],
-    [fsGUI.Button("Import (Select File)")],
-    [fsGUI.Button("Make new")],
-    [fsGUI.Button("Back")]
-]
+def createIoCLayout():
+    return [[fsGUI.Button("Import (Paste from clipboard)")],
+            [fsGUI.Button("Import (Select File)")],
+            [fsGUI.Button("Make new")],
+            [fsGUI.Button("<- Back")]]
 
 def main():
-    window = fsGUI.Window("IPAt", homeLayout, no_titlebar=True, grab_anywhere=True)
+    window = fsGUI.Window("IPAt", createHomeLayout(), no_titlebar=True, grab_anywhere=True, keep_on_top=True)
 
     while True:
         event, values = window.read()
-        if event == "Close":
+        if event == fsGUI.WIN_CLOSED or event == "Close":
+            window.close()
             break
 
         if event == "Import/Create a language config":
-            IoCwindow = fsGUI.Window("IPAt", importOrCreateLayout, no_titlebar=True, grab_anywhere=True)
+            IoCwindow = fsGUI.Window("IPAt", createIoCLayout(), no_titlebar=True, grab_anywhere=True, keep_on_top=True)
 
             while True:
                 IoCevent, IoCvalues = IoCwindow.read()
-                if event == "Close":
-                    break
 
-                if event == "Import (Paste from clipboard)":
+                if IoCevent == "Import (Paste from clipboard)":
                     clipboardData = pyperclip.paste()
                     
 
-                if event == "Import (Select File)":
+                if IoCevent == "Import (Select File)":
                     pass
                 
-                if event == "Make new":
+                if IoCevent == "Make new":
                     pass
 
-                if event == "Back":
+                if IoCevent == fsGUI.WIN_CLOSED or IoCevent == "<- Back":
+                    IoCwindow.close()
                     break
 
 
